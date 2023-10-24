3239e7460d7ec9d54eb7f32b7ea32e00584531f1
Otavio Barbosa <otaviomorcegao@gmail.com>
2023-10-23 23:08:27 -0300

    Updating env vars

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
 
 }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const SITE_ID = process.env.SITE_ID || 'MLB';
 
 app.get('/', (request, response) => {
